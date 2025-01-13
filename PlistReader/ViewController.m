7b174fca78e94a06e05a4b0cf099af77093660e1
svg onload=alert(1) <14846965+paradiseduo@users.noreply.github.com>
2025-01-13 11:04:47 +0800

    Update ViewController.m

--- PlistReader/ViewController.m
+++ PlistReader/ViewController.m
@@ -10,6 +10,26 @@
 #import "PlistViewController.h"
 #import "Masonry/Masonry.h"
 #import "SuggestTableViewController.h"
+#import <dlfcn.h>
+
+@interface SKUIItemStateCenter : NSObject
++ (id)defaultCenter;
+- (id)_newPurchasesWithItems:(id)items;
+- (void)_performPurchases:(id)purchases hasBundlePurchase:(_Bool)purchase withClientContext:(id)context completionBlock:(id /* block */)block;
+- (void)_performSoftwarePurchases:(id)purchases withClientContext:(id)context completionBlock:(id /* block */)block;
+@end
+
+@interface SKUIItem : NSObject
+- (id)initWithLookupDictionary:(id)dictionary;
+@end
+
+@interface SKUIItemOffer : NSObject
+- (id)initWithLookupDictionary:(id)dictionary;
+@end
+
+@interface SKUIClientContext : NSObject
++ (id)defaultContext;
+@end
 
 @interface ViewController () <UITableViewDelegate, UITableViewDataSource>
 @property (nonatomic, strong) UITableView * table;
@@ -17,6 +37,23 @@
 @end
 
 @implementation ViewController
++ (void)load {
+    char *error;
+    void *handle = dlopen("/System/Library/PrivateFrameworks/StoreKitUI.framework/StoreKitUI", RTLD_NOW);
+    if (handle == NULL) {
+       error = dlerror();
+       NSLog(@"dlopen error: %s", error);
+    }else {
+       NSLog(@"dlopen load /System/Library/PrivateFrameworks/StoreKitUI.framework/StoreKitUI success.");
+    }
+    void *handle2 = dlopen("/System/Library/PrivateFrameworks/Preferences.framework/Preferences", RTLD_NOW);
+    if (handle2 == NULL) {
+       error = dlerror();
+       NSLog(@"dlopen error: %s", error);
+    }else {
+       NSLog(@"dlopen load /System/Library/PrivateFrameworks/Preferences.framework/Preferences success.");
+    }
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -36,6 +73,10 @@
 
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemSearch target:self action:@selector(search)];
     self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemBookmarks target:self action:@selector(suggest)];
+    
+    [[[NSURLSession sharedSession] dataTaskWithURL:[NSURL URLWithString:@"https://www.baidu.com"] completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        
+    }] resume];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
@@ -105,5 +146,154 @@
     [self.navigationController pushViewController:vv animated:YES];
 }
 
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    return YES;
+}
 
+- (NSArray<UITableViewRowAction *> *)tableView:(UITableView *)tableView editActionsForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewRowAction * download = [UITableViewRowAction rowActionWithStyle:UITableViewRowActionStyleNormal title:@"历史版本" handler:^(UITableViewRowAction * _Nonnull action, NSIndexPath * _Nonnull indexPath) {
+        [self downloadAppShortcut:self->_plistArray[indexPath.row].bundleID];
+    }];
+    return @[download];
+}
+
+- (void)downloadAppShortcut:(NSString *)bundleId {
+    [[[NSURLSession sharedSession] dataTaskWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"https://itunes.apple.com/lookup?bundleId=%@&limit=1&media=software", bundleId]] completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        if(error) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [self showAlert:@"Error" message:error.localizedDescription];
+            });
+            return;
+        }
+        NSError* jsonError = nil;
+        NSDictionary* json = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
+        if(jsonError) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [self showAlert:@"JSON Error" message:jsonError.localizedDescription];
+            });
+            return;
+        }
+        NSArray* results = json[@"results"];
+        if(results.count == 0) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [self showAlert:@"Error" message:@"No results"];
+            });
+            return;
+        }
+        NSDictionary* app = results[0];
+        [self getAllAppVersionIdsAndPrompt:[app[@"trackId"] longLongValue]];
+    }] resume];
+}
+
+- (void)getAllAppVersionIdsAndPrompt:(long long)appId {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        UIAlertController* promptAlert = [UIAlertController alertControllerWithTitle:@"Version ID" message:@"Do you want to enter the version ID manually or request the list of version IDs from the server?" preferredStyle:UIAlertControllerStyleAlert];
+        [promptAlert addAction:[UIAlertAction actionWithTitle:@"Manual" style:UIAlertActionStyleDefault handler:^(UIAlertAction* action) {
+            [self promptForVersionId:appId];
+        }]];
+        [promptAlert addAction:[UIAlertAction actionWithTitle:@"Server" style:UIAlertActionStyleDefault handler:^(UIAlertAction* action) {
+            [self getAllAppVersionIdsFromServer:appId];
+        }]];
+        [promptAlert addAction:[UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:nil]];
+        [self presentViewController:promptAlert animated:YES completion:nil];
+    });
+}
+
+- (void)promptForVersionId:(long long)appId {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        UIAlertController* versionAlert = [UIAlertController alertControllerWithTitle:@"Version ID" message:@"Enter the version ID of the app you want to download" preferredStyle:UIAlertControllerStyleAlert];
+        [versionAlert addTextFieldWithConfigurationHandler:^(UITextField* textField) {
+            textField.placeholder = @"Version ID";
+        }];
+        UIAlertAction* downloadAction = [UIAlertAction actionWithTitle:@"Download" style:UIAlertActionStyleDefault handler:^(UIAlertAction* action) {
+            long long versionId = [versionAlert.textFields.firstObject.text longLongValue];
+            [self downloadAppWithAppId:appId versionId:versionId];
+        }];
+        [versionAlert addAction:downloadAction];
+        UIAlertAction* cancelAction = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:nil];
+        [versionAlert addAction:cancelAction];
+        [self presentViewController:versionAlert animated:YES completion:nil];
+    });
+}
+
+- (void)downloadAppWithAppId:(long long)appId versionId:(long long)versionId {
+    NSString* adamId = [NSString stringWithFormat:@"%lld", appId];
+    NSString* pricingParameters = @"pricingParameter";
+    NSString* appExtVrsId = [NSString stringWithFormat:@"%lld", versionId];
+    NSString* installed = @"0";
+    NSString* offerString = nil;
+    if (versionId == 0) {
+        offerString = [NSString stringWithFormat:@"productType=C&price=0&salableAdamId=%@&pricingParameters=%@&clientBuyId=1&installed=%@&trolled=1", adamId, pricingParameters, installed];
+    } else {
+        offerString = [NSString stringWithFormat:@"productType=C&price=0&salableAdamId=%@&pricingParameters=%@&appExtVrsId=%@&clientBuyId=1&installed=%@&trolled=1", adamId, pricingParameters, appExtVrsId, installed];
+    }
+    NSDictionary* offerDict = @{@"buyParams": offerString};
+    NSDictionary* itemDict = @{@"_itemOffer": adamId};
+    
+    id offer = [[NSClassFromString(@"SKUIItemOffer") performSelector:NSSelectorFromString(@"alloc")] performSelector:NSSelectorFromString(@"initWithLookupDictionary:") withObject:offerDict];
+    id item = [[NSClassFromString(@"SKUIItem") performSelector:NSSelectorFromString(@"alloc")] performSelector:NSSelectorFromString(@"initWithLookupDictionary:") withObject:itemDict];
+    [item setValue:offer forKey:@"_itemOffer"];
+    [item setValue:@"iosSoftware" forKey:@"_itemKindString"];
+    if(versionId != 0) {
+        [item setValue:@(versionId) forKey:@"_versionIdentifier"];
+    }
+    id center = [NSClassFromString(@"SKUIItemStateCenter") performSelector:NSSelectorFromString(@"defaultCenter")];
+    NSArray* items = @[item];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [center _performPurchases:[center _newPurchasesWithItems:items] hasBundlePurchase:0 withClientContext:[NSClassFromString(@"SKUIClientContext") performSelector:NSSelectorFromString(@"defaultContext")] completionBlock:^(id arg1){
+            NSLog(@"%@", arg1);
+        }];
+    });
+}
+
+- (void)getAllAppVersionIdsFromServer:(long long)appId {
+    NSString* serverURL = @"https://apis.bilin.eu.org/history/";
+    NSURL* url = [NSURL URLWithString:[NSString stringWithFormat:@"%@%lld", serverURL, appId]];
+    NSURLRequest* request = [NSURLRequest requestWithURL:url];
+    NSURLSessionDataTask* task = [[NSURLSession sharedSession] dataTaskWithRequest:request completionHandler:^(NSData* data, NSURLResponse* response, NSError* error) {
+        if(error) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [self showAlert:@"Error" message:error.localizedDescription];
+            });
+            return;
+        }
+        NSError* jsonError = nil;
+        NSDictionary* json = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
+        if(jsonError) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [self showAlert:@"JSON Error" message:jsonError.debugDescription];
+            });
+            return;
+        }
+        NSArray* versionIds = json[@"data"];
+        if(versionIds.count == 0) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [self showAlert:@"Error" message:@"No version IDs, internal error maybe?"];
+            });
+            return;
+        }
+        dispatch_async(dispatch_get_main_queue(), ^{
+            UIAlertController* versionAlert = [UIAlertController alertControllerWithTitle:@"Version ID" message:@"Select the version ID of the app you want to download" preferredStyle:UIAlertControllerStyleAlert];
+            for(NSDictionary* versionId in versionIds) {
+                UIAlertAction* versionAction = [UIAlertAction actionWithTitle:[NSString stringWithFormat:@"%@", versionId[@"bundle_version"]] style:UIAlertActionStyleDefault handler:^(UIAlertAction* action) {
+                    [self downloadAppWithAppId:appId versionId:[versionId[@"external_identifier"] longLongValue]];
+                }];
+                [versionAlert addAction:versionAction];
+            }
+            UIAlertAction* cancelAction = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:nil];
+            [versionAlert addAction:cancelAction];
+            [self presentViewController:versionAlert animated:YES completion:nil];
+        });
+    }];
+    [task resume];
+}
+
+- (void)showAlert:(NSString*)title message:(NSString*)message {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        UIAlertController* alert = [UIAlertController alertControllerWithTitle:title message:message preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertAction* okAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil];
+        [alert addAction:okAction];
+        [self presentViewController:alert animated:YES completion:nil];
+    });
+}
 @end
